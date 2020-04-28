3ec5bdd4d4e2ac9102544d4b63e6597a0c088707
Aditya Pratama Nusantara <adityaadit677@gmail.com>
2020-04-28 19:14:30 +0700

    Update k-mean.R

--- Data Science in Marketing Customer Segmentation/k-mean.R
+++ Data Science in Marketing Customer Segmentation/k-mean.R
@@ -1,15 +1,47 @@
 #Bagian Data Preparation
+
 pelanggan <- read.csv("https://academy.dqlab.id/dataset/customer_segments.txt", sep="\t")
+
 pelanggan_matrix <- data.matrix(pelanggan[c("Jenis.Kelamin", "Profesi", "Tipe.Residen")])
+
 pelanggan <- data.frame(pelanggan, pelanggan_matrix)
+
 Profesi <- unique(pelanggan[c("Profesi","Profesi.1")])
+
 Jenis.Kelamin <- unique(pelanggan[c("Jenis.Kelamin","Jenis.Kelamin.1")])
+
 Tipe.Profesi <- unique(pelanggan[c("Tipe.Residen","Tipe.Residen.1")])
+
 pelanggan$NilaiBelanjaSetahun <- pelanggan$NilaiBelanjaSetahun/1000000
+
 field_yang_digunakan = c("Jenis.Kelamin.1", "Umur", "Profesi.1", "Tipe.Residen.1","NilaiBelanjaSetahun")
+
 #Bagian K-Means
+
 set.seed(100)
-#fungsi kmeans untuk membentuk 5 cluster dengan 25 skenario random dan simpan ke dalam variable segmentasi
-segmentasi <- kmeans(x=pelanggan[c("Umur","Profesi.1")], centers=5, nstart=25)
-#tampilkan hasil k-means
-segmentasi
+
+segmentasi <- kmeans(x=pelanggan[field_yang_digunakan], centers=5, nstart=25)
+
+#Penggabungan hasil cluster
+
+segmentasi$cluster
+
+pelanggan$cluster <- segmentasi$cluster
+
+#menampilkan struktur
+str(pelanggan)
+
+#Analisa hasil
+#Filter cluster ke-1
+which(pelanggan$cluster == 1)
+length(which(pelanggan$cluster == 1))
+#Filter cluster ke-1
+which(pelanggan$cluster == 2)
+length(which(pelanggan$cluster == 2))
+
+#Melihat data cluster ke-1
+pelanggan[which(pelanggan$cluster == 1),]
+
+segmentasi$withinss
+segmentasi$cluster
+segmentasi$tot.withinss
